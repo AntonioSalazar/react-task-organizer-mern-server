a69db2d34c6022123bb87ad4c058c5135b79bb2d
AntonioSE <antonio.salazar@oliverwyman.com>
2020-07-14 11:48:58 -0500

    saving latest changes

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,12 @@
 //Routes used to create users
+const express = require('express');
+const router = express.Router();
+
+//Create a user
+//----> api/users
+
+router.post("/", () => {
+
+});
+
+module.exports = router;

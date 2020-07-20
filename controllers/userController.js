eae067156e122eb2bbd01463ee85e1701a5c118b
AntonioSE <antonio.salazar@oliverwyman.com>
2020-07-19 22:34:18 -0500

    added validation to the creation of the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,30 @@
+const User = require('../models/User')
+
+exports.createUser = async (req, res) => {
+
+    //extract email and password
+    const { email, password } = req.body;
+    
+    try {
+
+        //validate user is unique
+        let user = await User.findOne({email});
+
+        if(user) {
+            return res.status(400).json({msg: "User already exists"})
+        }
+
+        //create the new user into DB
+        user = new User(req.body);
+
+        //save the new user
+        await user.save()
 
 
-exports.createUser = (req, res) => {
-    console.log(req.body);
+        //confirmation messaage
+        res.json({msg: "User has been created succesfully"})
+    } catch (error) {
+        console.log(error);;
+        res.status(400).send('There was an error, we were not able to save the new user information')
+    }
 }

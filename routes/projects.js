252c9b6c0bf1760270d711588040339919f1b376
unknown <antonio.salazar.emrsn@gmail.com>
2020-07-26 21:46:38 -0500

    able to create new projects into DB

---
+++ routes/projects.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//controller
+const projectController = require('../controllers/projectController');
+
+//Create projects
+//api/projects
+
+router.post("/",
+    projectController.createProject    
+)
+
+module.exports = router;

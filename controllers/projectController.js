3fab67b4138b26317b570151846b2b691d8a7b93
unknown <antonio.salazar.emrsn@gmail.com>
2020-07-26 22:32:43 -0500

    checking errors for projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,14 @@
 const Project = require('../models/Project');
+const { validationResult } = require('express-validator')
 
 exports.createProject = async (req, res) => {
+
+    //check if there are any errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
 
     try {
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 //controller
 const projectController = require('../controllers/projectController');
 
-const auth = require('../middleware/auth');
 
 //Create projects
 //api/projects
 
 router.post("/",
     auth,
+    [
+        check('name', 'Project name is mandatory').not().isEmpty()
+    ],  
     projectController.createProject    
 )
 
